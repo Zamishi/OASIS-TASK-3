4b47d3419e317610a2c469a0edf97ebf2758e0c1
Zamishi <amishi140596@gmail.com>
2024-08-31 20:34:14 +0530

    Update DashboardController.java

--- src/main/java/com/example/demo/controller/DashboardController.java
+++ src/main/java/com/example/demo/controller/DashboardController.java
@@ -9,7 +9,6 @@ public class DashboardController {
     
     @GetMapping("/dashboard")
     public String dashboard(Model model) {
-        // You can add user-specific details here, if needed
         return "dashboard";
     }
 }
